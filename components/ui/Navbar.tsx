f8e8dcc36ae2973d35cfdf2e9c24ac244abd90d7
Johan Altamar <johan.altamar.dev@gmail.com>
2022-03-20 15:12:13 -0500

    Add(-): link to home page added

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,18 +1,21 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export const Navbar = () => {
   return (
     <div className="flex items-center justify-between w-full p-0 px-5 bg-red-600 dark:bg-gray-800 text-white">
-      <span className="text-2xl flex items-center">
-        <Image
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-          alt="app icon"
-          width={70}
-          height={70}
-        />
-        <span className="text-3xl">P</span>okémon
-      </span>
+      <Link href="/" passHref>
+        <div className="text-2xl flex items-center hover:cursor-pointer">
+          <Image
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+            alt="app icon"
+            width={70}
+            height={70}
+          />
+          <span className="text-3xl">P</span>okémon
+        </div>
+      </Link>
       <span>Favorites</span>
     </div>
   );
