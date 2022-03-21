9ea96ea5a419ae9cf3538b943a80bd551417dfb6
Johan Altamar <johan.altamar.dev@gmail.com>
2022-03-20 22:25:44 -0500

    Add(-): Links updated to pokemon name

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -6,7 +6,7 @@ import { SmallPokemon } from "@/interfaces";
 
 export const PokemonCard: React.FC<SmallPokemon> = ({ id, name, img }) => {
   return (
-    <Link href={`/pokemon/${id}`} passHref>
+    <Link href={`/pokemon/name/${name}`} passHref>
       <div className="col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-2 2xl:col-span-1 p-4 rounded-lg shadow-md hover:shadow-lg hover:cursor-pointer transition-all flex flex-col">
         <Image src={img} alt={name} width={140} height={140} />
         <div className="flex justify-between">
